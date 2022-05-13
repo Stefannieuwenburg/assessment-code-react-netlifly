5d71196ae5504c3fac96f3754d074f74d4dbab92
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-05-13 22:09:12 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
